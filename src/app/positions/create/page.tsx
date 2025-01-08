effa4d8fe83f3bfcb868986eee38ffbf72fed3c4
taayyohh <milesxbridges@gmail.com>
2025-01-08 09:42:11 -0500

    update liqudiity

--- src/app/positions/create/page.tsx
+++ src/app/positions/create/page.tsx
@@ -11,7 +11,7 @@ export default function Pool() {
       >
         {/*<CreatePair />*/}
         <AddLiquidityStepper
-          defaultTokenA="0x56deBC624a43c84479b79d2Dfe4Db9612C1AFe61"
+          defaultTokenA="0xBa71efd94be63bD47B78eF458DE982fE29f552f7"
           defaultTokenB="0x223f1397e9D1250C291a568E2F82601c62c14560"
         />
       </View>
