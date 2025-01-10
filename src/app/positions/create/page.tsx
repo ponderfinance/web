1d25b591c05f6fe63aa293bed8fad7d06901a772
taayyohh <milesxbridges@gmail.com>
2025-01-10 14:44:32 -0500

    update contribution card

--- src/app/positions/create/page.tsx
+++ src/app/positions/create/page.tsx
@@ -8,8 +8,8 @@ export default function Pool() {
       <View paddingInline={16} maxWidth={'1280px'}>
         {/*<CreatePair />*/}
         <AddLiquidityStepper
-          defaultTokenA="0x9c3ae329a6BcCd5d18c45a80d01f8f149a73D3a9"
-          defaultTokenB="0xBa71efd94be63bD47B78eF458DE982fE29f552f7"
+          defaultTokenA="0x9F181EB556Afb162002604bA2dC7c83bc11D6cbE"
+          defaultTokenB="0x9c3ae329a6BcCd5d18c45a80d01f8f149a73D3a9"
         />
       </View>
     </View>
