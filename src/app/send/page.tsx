88743768b8b499dfb57c56d93a28e547dcbc2211
taayyohh <milesxbridges@gmail.com>
2025-01-09 22:32:01 -0500

    update send

--- src/app/send/page.tsx
+++ src/app/send/page.tsx
@@ -6,7 +6,7 @@ export default function Swap() {
   return (
     <View direction="column">
       <View insetTop={36}>
-        <Send defaultTokenToSend="0x9c3ae329a6BcCd5d18c45a80d01f8f149a73D3a9" />
+        <Send defaultTokenToSend="0xBa71efd94be63bD47B78eF458DE982fE29f552f7" />
       </View>
     </View>
   )
