1fefc9dbee99a38e3b4cb077dec0bfbf3e85082f
taayyohh <milesxbridges@gmail.com>
2025-05-09 11:52:16 -0400

    update

--- relay.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  src: "./src",  // Point to src directory
-  schema: "./schema.graphql",
-  exclude: ["**/node_modules/**", "**/__generated__/**", "**/.next/**"],
-  language: "typescript",
-  artifactDirectory: "./src/__generated__"
-};
