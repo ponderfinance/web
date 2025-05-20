7f8c1ed8e4f97127eb7d85025a9c6deb220a4d18
taayyohh <milesxbridges@gmail.com>
2025-05-20 06:48:11 -0400

    update token

--- src/utils/ipfs.ts
+++ src/utils/ipfs.ts
@@ -1,3 +1,3 @@
 export const getIpfsGateway = (uri: string): string => {
-  return uri?.replace('ipfs://', 'https://ponder-finance.mypinata.cloud/ipfs/')
+  return uri?.replace('ipfs://', 'https://ponder-finance.mypinata.cloud/ipfs/') + '?pinataGatewayToken=0jL5Qyjh-XEoTm-JHEX3Ye_IXh8eNUJA5X5CO9G98PSLYiE_-3KaxZXjR5rySbIz'
 }
